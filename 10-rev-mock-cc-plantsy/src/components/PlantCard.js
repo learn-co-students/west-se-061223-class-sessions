704da7c601afd930a58cc21826c29e7b13fef6a3
betalantz <betalantz@gmail.com>
2023-07-12 14:49:59 -0700

    complete deliv 3

--- 10-rev-mock-cc-plantsy/src/components/PlantCard.js
+++ 10-rev-mock-cc-plantsy/src/components/PlantCard.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({id, image = "https://via.placeholder.com/400", name, price}) {
+  const [isInStock, setIsInStock] = useState(true)
+
+  const toggleInStock = () => setIsInStock(isInStock => !isInStock)
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button onClick={toggleInStock} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={toggleInStock} >Out of Stock</button>
       )}
     </li>
   );
