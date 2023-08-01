19863c236134bb86930c604cca9d073b53eb4669
betalantz <betalantz@gmail.com>
2023-08-01 14:42:44 -0700

    Customer propty name

---
+++ 12-mock-cc-coffee-shop/lib/classes/customer.py
@@ -0,0 +1,23 @@
+from classes.order import Order
+
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise Exception("name is invalid")
+
+    def orders(self):
+        pass
+
+    def coffees(self):
+        pass
