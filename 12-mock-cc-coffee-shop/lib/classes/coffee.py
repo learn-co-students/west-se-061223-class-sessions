efa55becdfd516b0ec5409110ff75f7bec1a0e51
betalantz <betalantz@gmail.com>
2023-08-01 15:07:30 -0700

    Coffee orders complete

--- 12-mock-cc-coffee-shop/lib/classes/coffee.py
+++ 12-mock-cc-coffee-shop/lib/classes/coffee.py
@@ -1,17 +1,29 @@
+from classes.order import Order
+
+
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
-    def orders(self, new_order=None):
-        from classes.order import Order
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and not hasattr(self, "name"):
+            self._name = value
+        else:
+            raise Exception("invalid operation")
+
+    def orders(self):
+        return [order for order in Order.all if order.coffee == self]
+
+    def customers(self):
         pass
-    
-    def customers(self, new_customer=None):
-        from classes.customer import Customer
-        pass
-    
+
     def num_orders(self):
         pass
-    
+
     def average_price(self):
         pass
--- 12-mock-cc-coffee-shop/lib/debug.py
+++ 12-mock-cc-coffee-shop/lib/debug.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import ipdb
-
+from classes.coffee import Coffee
 from classes.customer import Customer
 from classes.order import Order
-from classes.coffee import Coffee
 
-if __name__ == '__main__':
-    print("HELLO! :) let's debug")
+if __name__ == "__main__":
+    barbie = Customer("Barbie")
+    ken = Customer("Ken")
+    mocha = Coffee("Mocha")
+    latte = Coffee("Latte")
+    o1 = Order(barbie, latte, 4)
+    o2 = Order(barbie, mocha, 5)
+    o3 = Order(ken, mocha, 5)
 
     ipdb.set_trace()
