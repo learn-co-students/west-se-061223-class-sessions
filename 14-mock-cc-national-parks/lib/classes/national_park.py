93559e24ec2b7ccdff24614e606a551d6ea1ef9b
betalantz <betalantz@gmail.com>
2023-08-02 13:50:35 -0700

    init and propty all classes

--- 14-mock-cc-national-parks/lib/classes/national_park.py
+++ 14-mock-cc-national-parks/lib/classes/national_park.py
@@ -5,6 +5,17 @@ class NationalPark:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and not hasattr(self, "name"):
+            self._name = name
+        else:
+            raise Exception
+
     def trips(self):
         pass
 
--- 14-mock-cc-national-parks/lib/debug.py
+++ 14-mock-cc-national-parks/lib/debug.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import ipdb
-
 from classes.national_park import NationalPark
-from classes.visitor import Visitor
 from classes.trip import Trip
+from classes.visitor import Visitor
 
-if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
+if __name__ == "__main__":
+    p1 = NationalPark("Yosemmette")
+    vis = Visitor("Bill")
+    vis2 = Visitor("Steve")
+    t_1 = Trip(vis, p1, "May 5th", "May 9th")
 
-    # ipdb.set_trace()
+    ipdb.set_trace()
