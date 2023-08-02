c42d4b81907d9a9feff677522ae3fd452a3d1070
betalantz <betalantz@gmail.com>
2023-08-02 09:00:20 -0700

    up to relationship methods

--- 13-mock-cc-game-tracker/lib/classes/game.py
+++ 13-mock-cc-game-tracker/lib/classes/game.py
@@ -5,6 +5,17 @@ class Game:
     def __init__(self, title):
         self.title = title
 
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and len(title) > 0 and not hasattr(self, "title"):
+            self._title = title
+        else:
+            raise Exception
+
     def results(self):
         pass
 
--- 13-mock-cc-game-tracker/lib/classes/player.py
+++ 13-mock-cc-game-tracker/lib/classes/player.py
@@ -6,12 +6,26 @@ class Player:
 
     def __init__(self, username):
         self.username = username
+        Player.all.append(self)
+
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, username):
+        if type(username) == str and 2 <= len(username) <= 16:
+            self._username = username
+        else:
+            raise Exception
 
     def results(self):
-        pass
+        return [
+            result for result in Result.all if result.player == self
+        ]  # like a filter
 
     def games_played(self):
-        pass
+        return [result.game for result in self.results()]  # like a map
 
     def played_game(self, game):
         pass
