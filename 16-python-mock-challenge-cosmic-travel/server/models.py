996c76cd356a217a371efa5f3c3af29a7641de7d
betalantz <betalantz@gmail.com>
2023-08-22 09:04:17 -0700

    models passing tests

--- 16-python-mock-challenge-cosmic-travel/server/models.py
+++ 16-python-mock-challenge-cosmic-travel/server/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 
 convention = {
@@ -14,7 +14,7 @@ convention = {
 
 metadata = MetaData(naming_convention=convention)
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy(metadata=metadata, engine_options={"echo": True})
 
 
 class Planet(db.Model, SerializerMixin):
@@ -25,9 +25,13 @@ class Planet(db.Model, SerializerMixin):
     distance_from_earth = db.Column(db.Integer)
     nearest_star = db.Column(db.String)
 
-    # Add relationship
+    # missions (has_many)
+    missions = db.relationship("Mission", backref="planet", cascade="delete")
+    # scientists (has_many, through missions)
+    scientists = association_proxy("missions", "scientist")
 
     # Add serialization rules
+    serialize_rules = ("-missions.planet", "-missions.scientist",)
 
 
 class Scientist(db.Model, SerializerMixin):
@@ -37,11 +41,20 @@ class Scientist(db.Model, SerializerMixin):
     name = db.Column(db.String)
     field_of_study = db.Column(db.String)
 
-    # Add relationship
+    # missions (has_many)
+    missions = db.relationship("Mission", backref="scientist", cascade="delete")
+    # planets (has_many through missions)
+    planets = association_proxy("missions", "planet")
 
     # Add serialization rules
+    serialize_rules = ("-missions.planet", "-missions.scientist",)
 
     # Add validation
+    @validates('name', 'field_of_study')
+    def validate_name_field_of_study(self, key, value):
+        if not value:
+            raise ValueError("Name or field of study cannot be blank")
+        return value
 
 
 class Mission(db.Model, SerializerMixin):
@@ -49,12 +62,28 @@ class Mission(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
 
-    # Add relationships
+    # scientist (belongs_to, assoc. method created by backref)
+    # planet (belongs_to, assoc. method created by backref)
 
     # Add serialization rules
 
     # Add validation
+    @validates("name")
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Name cannot be blank")
+        return name
+    
+    @validates("planet_id", "scientist_id")
+    def validate_foreign_keys(self, key, value):
+        if not value:
+            raise ValueError("Must have foreign keys for scientist and planet")
+        return value
+    
 
 
 # add any models you may need.
