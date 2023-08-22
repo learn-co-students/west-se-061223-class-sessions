df258084c8bfa91f3d89f5a9c87b1dde22ea62a9
betalantz <betalantz@gmail.com>
2023-08-22 14:11:54 -0700

    create and seed tables

--- 17-python-mock-challenge-camping-fun/server/models.py
+++ 17-python-mock-challenge-camping-fun/server/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 
 convention = {
@@ -9,61 +9,63 @@ convention = {
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
+    "pk": "pk_%(table_name)s",
 }
 
 metadata = MetaData(naming_convention=convention)
 
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy(metadata=metadata, engine_options={"echo": True})
 
 
 class Activity(db.Model, SerializerMixin):
-    __tablename__ = 'activities'
+    __tablename__ = "activities"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
 
     # Add relationship
-    
+
     # Add serialization rules
-    
+
     def __repr__(self):
-        return f'<Activity {self.id}: {self.name}>'
+        return f"<Activity {self.id}: {self.name}>"
 
 
 class Camper(db.Model, SerializerMixin):
-    __tablename__ = 'campers'
+    __tablename__ = "campers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
 
     # Add relationship
-    
+
     # Add serialization rules
-    
+
     # Add validation
-    
-    
+
     def __repr__(self):
-        return f'<Camper {self.id}: {self.name}>'
+        return f"<Camper {self.id}: {self.name}>"
 
 
 class Signup(db.Model, SerializerMixin):
-    __tablename__ = 'signups'
+    __tablename__ = "signups"
 
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.Integer)
 
+    camper_id = db.Column(db.Integer, db.ForeignKey("campers.id"))
+    activity_id = db.Column(db.Integer, db.ForeignKey("activities.id"))
+
     # Add relationships
-    
+
     # Add serialization rules
-    
+
     # Add validation
-    
+
     def __repr__(self):
-        return f'<Signup {self.id}>'
+        return f"<Signup {self.id}>"
 
 
 # add any models you may need.
